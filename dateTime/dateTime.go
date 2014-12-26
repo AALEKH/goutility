16b6138261c6e8f5540567d1b1eefc2d06832c80
aalekh <aalekh1993@rediffmail.com>
2014-12-27 03:43:20 +0530

    Started working on date script

---
+++ dateTime/dateTime.go
@@ -0,0 +1,19 @@
+/*
+Author: AAlekh Nigam@ aalekh.nigam@gmail.com 
+*/
+package goutility
+
+import (
+	"fmt"
+	"time"
+)
+
+type Date struct {
+	day int
+	month int
+	year int
+}
+
+func CurrentDate() Date{
+	//Get current date time and year seperately
+}
